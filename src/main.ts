79e24db6c83cffa4cb6d805f1616f22423ae2c84
Ta-Run <tarunsharma11091999@gmail.com>
2025-07-25 23:05:43 +0530

    fix server command for

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('server run starr')
   await app.listen(3000);
 }
 bootstrap();
